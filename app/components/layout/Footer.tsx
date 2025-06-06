7fda07f4a0cfbfc1b59408756b61deb542c6a668
yassinagx <yassin.agx@gmail.com>
2025-06-06 14:09:25 +0200

    feat(Footer): Create Footer component

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const footerItems = ["Mentions légales", "CGU", "Plan du site", "License Creative Commons"];
+const zigZagStyle = {
+  borderImageSource: 'url("/zigzags/zigzag-footer.svg")',
+  borderImageSlice: 52,
+  borderImageRepeat: 'repeat',
+}
+
+export default function Footer() {
+  return (
+    <footer className="bg-[#753345] grid grid-cols-12 text-white">
+      <div className="col-span-4 px-8 pt-4 pb-8">
+        <ul>
+          {
+            ["Mentions légales", "CGU", "Plan du site", "License Creative Commons"].map((item) => (
+              <li className='mb-2 text-lg' key={item}>
+                <Link href="#" className='mb-2 text-lg'>{item}</Link>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+      <div className="col-span-3 border-l-14 border-white border-dashed px-8 pt-4 pb-8"
+        style={zigZagStyle}
+      >
+        <ul>
+          <li className='mb-2 text-lg'>
+            <Link href="#" className='mb-2 text-lg'>Abonner-vous à la newsletter</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="col-span-3 border-l-14 border-white border-dashed px-8 pt-4 pb-8"
+        style={zigZagStyle}
+      >
+        <ul>
+          {
+            ["Documentation", "Lexique", "Presse"].map((item) => (
+              <li className='mb-2 text-lg' key={item}>
+                <Link href="#" className='mb-2 text-lg'>{item}</Link>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+      <div className="col-span-2 border-l-14 border-white border-dashed text-[#ffed38] px-8 pt-4 pb-8"
+        style={zigZagStyle}
+      >
+        <Image src="/arrows/arrow-footer.svg" alt="Arrow back to top"
+          width={1}
+          height={1}
+          style={{ height: 'auto', width: '25%' }}
+          className="mx-auto" />
+
+        <p className='text-center mt-3'>Retour en haut</p>
+      </div>
+    </footer>
+  );
+}
---
+++ stories/Footer.stories.tsx
@@ -0,0 +1,14 @@
+import { expect } from 'storybook/test';
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import Footer from '@/app/components/layout/Footer';
+import "@/app/globals.css";
+
+const meta = {
+  component: Footer,
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+type Story = StoryObj<typeof Footer>;
+
+export const Default: Story = {
+};
