5f60e6de401fa9adc7ee0f6aa0563ed343aecad8
amare16 <amarenighatu16@gmail.com>
2025-06-11 16:20:04 +0200

    feat(buttons): create buttons component

---
+++ app/components/ui/ButtonDownload.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export type ButtonProps = {
+    label: string;
+    url: string;
+}
+
+export default function ButtonDownload({ label, url }: ButtonProps) {
+  return (
+    <button>
+    <Link
+      download
+      href={url}
+      className="rounded-full transition duration-200 w-64
+        grid grid-cols-6 gap-0 items-center place-items-center text-base
+        bg-bees-400 hover:bg-bees-200 py-4 px-6 group"
+    >
+      <div className="col-span-5">
+        <p className="text-center font-medium px-2">{label}</p>
+      </div>
+      <div className="col-span-1 text-center relative w-9 h-9">
+        {/* Icône normale */}
+        <Image
+          src="/arrows/arrow-right-icon.png"
+          alt="icon"
+          width={36}
+          height={36}
+          className="rotate-90 absolute inset-0 transition-opacity duration-200 opacity-100 group-hover:opacity-0"
+        />
+        {/* Icône au survol */}
+        <Image
+          src="/arrows/arrow-right-zigzag-icon.png"
+          alt="icon hover"
+          width={36}
+          height={36}
+          className="rotate-90 absolute inset-0 transition-opacity duration-200 opacity-0 group-hover:opacity-100"
+        />
+      </div>
+    </Link>
+    </button>
+  );
+}
---
+++ app/components/ui/ButtonLink.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export type ButtonProps = {
+    label: string;
+    url: string;
+}
+
+
+export default function ButtonLink({ label, url}: ButtonProps) {
+
+  return (
+    <button>
+      <Link className="rounded-full transition duration-200 w-64 group
+       grid grid-cols-6 gap-0 items-center place-items-center text-base bg-lavender-400 hover:bg-lavender-200 py-4 px-6" href={url} >
+        <div className="col-span-5">
+          <p className="text-center font-medium px-2">{label}</p>
+        </div>
+      <div className="col-span-1 text-center relative w-9 h-9">
+        {/* Icône normale */}
+        <Image
+          src="/arrows/arrow-right-icon.png"
+          alt="icon"
+          width={36}
+          height={36}
+          className="absolute inset-0 transition-opacity duration-200 opacity-100 group-hover:opacity-0"
+        />
+        {/* Icône au survol */}
+        <Image
+          src="/arrows/arrow-right-zigzag-icon.png"
+          alt="icon hover"
+          width={36}
+          height={36}
+          className="absolute inset-0 transition-opacity duration-200 opacity-0 group-hover:opacity-100"
+        />
+        </div>
+      </Link>
+    </button>
+  );
+}
